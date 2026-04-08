c0243f8f6f8895cd39b6ca6b244dbbd524e835fb
jonilala796 <17726681+jonilala796@users.noreply.github.com.>
2026-04-08 23:45:52 +0200

    feat: Add DiscoverDevices dialog and AuthTokenMaskConverter for Nanoleaf integration

---
+++ Artemis.Plugins.Devices.Nanoleaf/Views/Dialogs/DiscoverDevicesDialogView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Artemis.Plugins.Devices.Nanoleaf.ViewModels.Dialogs;
+using Artemis.UI.Shared;
+
+namespace Artemis.Plugins.Devices.Nanoleaf.Views.Dialogs;
+
+public partial class DiscoverDevicesDialogView : ReactiveAppWindow<DiscoverDevicesDialogViewModel>
+{
+    public DiscoverDevicesDialogView()
+    {
+        InitializeComponent();
+    }
+}
